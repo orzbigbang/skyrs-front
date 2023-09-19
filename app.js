8d3fc66f2d7f11e483de83fedc14467115218072
温老师 <13221791+shenvinc@user.noreply.gitee.com>
2023-09-19 22:24:21 +0900

    nihao

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(require('cors')())
 
-// app.get('/', () => {})
-
-const port = 3007
-app.listen(port, () => {
+const port = 80
+const host = "127.0.0.1"
+app.listen(port, host, () => {
     console.log(`server runing at 127.0.0.1:${port}`)
 })
