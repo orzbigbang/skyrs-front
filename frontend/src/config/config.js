cca59d3f95cc282615f881c4a331d75325b9443d
温老师 <13221791+shenvinc@user.noreply.gitee.com>
2023-12-10 20:48:52 +0900

    nihao

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,9 +1,9 @@
 export const baseURL = "//localhost:8000/api"
 
-export const mediaURL = "//13.112.28.13:8000"
+export const mediaURL = "//localhost:8000/media/estate/"
 
 export const atbbURL = "//localhost:8000"
-
+// 35.72.6.170
 export const apiURL = {
     estate: `${baseURL}/estate`,
     addFavorate: `${baseURL}/favorite/`,
@@ -11,10 +11,6 @@ export const apiURL = {
     query: `${baseURL}/contact_info/`,
     upload: `${baseURL}/estate-upload/`,
     count: `${baseURL}/count/`,
-    // atbb: `${baseURL}/atbb/`,
-    // atbb_bm: `http://35.72.6.170/atbb/buy_mansion`,
-    // atbb_bo: `http://35.72.6.170/atbb/buy_one`,
-    // atbb_rmo: `http://35.72.6.170/atbb/rent_mansion_one`,
     atbb_bm: `${atbbURL}/atbb/buy_mansion`,
     atbb_bo: `${atbbURL}/atbb/buy_one`,
     atbb_rmo: `${atbbURL}/atbb/rent_mansion_one`,
